39359094532505fcb16dfd777325aa5a054ab224
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-29 21:43:56 -0800

    cowsay

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
-console.log('woot woot');
+import React from 'react';
+import ReactDom from 'react-dom';
+import {say} from 'cowsay';
 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        content: 'mooooo',
+      }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Peeps</h1>
+        <button> click me </button>
+      </div>
+    )
+  }
+}
+
+let poo = say({text:'hi'});
+
+
+ReactDom.render(<App />, document.getElementById('root'));
