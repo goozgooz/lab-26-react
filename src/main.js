29933eb6f39fda566422f00eed75fba5316c11aa
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-11-30 16:08:19 -0800

    lab requirements done

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
+import './style/main.scss';
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import {say} from 'cowsay';
+import faker from 'faker';
 
 
 class App extends React.Component {
@@ -9,20 +12,24 @@ class App extends React.Component {
       this.state = {
         content: 'mooooo',
       }
-  }
+    
+    this.handleCow = this.handleCow.bind(this);
+  };
 
+  handleCow () {
+    this.setState({content: faker.company.bsBuzz()});
+  };
 
   render() {
     return (
       <div>
         <h1>Hello Peeps</h1>
-        <button> click me </button>
+        <button onClick={this.handleCow}> click me </button>
+        <div className='cow'> {say({text: this.state.content})} </div>
       </div>
     )
   }
 }
-
-let poo = say({text:'hi'});
 
 
 ReactDom.render(<App />, document.getElementById('root'));
